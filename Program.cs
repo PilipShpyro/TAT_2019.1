d65dc39e0455fe0151bb7d749b456b659ded8078
PilipShpyro <48410816+PilipShpyro@users.noreply.github.com>
2019-03-13 17:48:00 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
     public class Program
+    {
+        public static void Main(string[] args)
         {
-            public class SubstrSearch
+            try
             {
-                private static string str;
-                public void getStr(string _str)
+
+                if (args.Length > 0 && args[0].Length > 2)
                 {
-                    str = _str;
-                }
-                public bool twoElementsDetect(string x)
-                {
-                    for (int t = 0; t < x.Length - 1; t++)
+                    SearchAndDisplay a = new SearchAndDisplay();
+                    for (int i = 0; i < args.Length; i++)
                     {
-                        if (x[t] == x[t + 1]) return true;
-                    }
-                    return false;
-                }
-                public void search()
-                {
-                    for (int i = 0; i < str.Length; i++)
-                    {
-                        for (int k = 2; k < str.Length - i + 1; k++)
-                        {
-                            if (twoElementsDetect(str.Substring(i, k))) break;
-                            Console.WriteLine(str.Substring(i, k));
-                        }
+                        a.GetStr(args[i]);
+                        a.Work();
                     }
                 }
             }
-            public static void Main(string[] args)
+            catch(Exception e)
             {
-            if (args.Length > 0 && args[0].Length > 2)
-            {
-                for (int i = 0; i < args.Length; i++)
-                {
-                    SubstrSearch a = new SubstrSearch();
-                    a.getStr(args[i]);
-                    a.search();
-                }
+                Console.WriteLine("Error {0}.", e.Message);
             }
         }
     }
