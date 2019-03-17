8c93fad7494052657e9fb976f5cd13f7f46002e9
PilipShpyro <48410816+PilipShpyro@users.noreply.github.com>
2019-03-17 17:09:41 +0300

    Add files via upload

---
+++ Word2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Word
+    {
+        int emphasis;
+        string word;
+        string str;
+        public void SetStr(string str)
+        {
+            this.str = str;
+            ConvertFromStr(str);
+        }
+        public string GetStr()
+        {
+            return str;
+        }
+        public string GetWord()
+        {
+            return word;
+        }
+        public void SetEmphasis(int emphasis)
+        {
+            this.emphasis = emphasis;
+        }
+        public int GetEmphasis()
+        {
+            emphasis = str.IndexOf('+');
+            return emphasis;
+        }
+        private void ConvertFromStr(string str)
+        {
+            emphasis = str.IndexOf('+');
+            if(emphasis > -1)
+            {
+                word = str.Remove(emphasis, 1);
+            }
+        }
+    }
+}
